5b508b468ed8f94280a4c542a8bcb9d56e8758ea
Margarita Korolkova <mkorolkova311@gmail.com>
2021-01-16 13:36:10 +0300

    testng commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,11 +1,28 @@
 package ru.netology.service;
 
-public class CashbackHackServiceTest {
-        private final int boundary = 1000;
 
-        public int remain(int amount) {
-            return boundary - amount % boundary;
-        }
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = service.remain(900);
+        Assert.assertEquals(100,boundary);
     }
 
+    @Test
+    public void shouldAmountEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = service.remain(1000);
+        Assert.assertEquals(0,boundary);
+    }
 
+    @Test
+    public void shouldAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int boundary = service.remain(1215);
+        Assert.assertEquals(785, boundary);
+    }
+}
